7d8a448ba4fffe1d0caa7a148806b630031ea4c9
Sally Resch <sallyresch27@gmail.com>
2022-05-21 11:51:29 +0200

    Linked to home page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function FirstPost() {
   return (
     <div>
-      <h1>First Post</h1>
       <h2>
         <Link href='/'>
           <a>Back to Homepage</a>
         </Link>
       </h2>
+      <h1>First Post</h1>
     </div>
   )
 }
