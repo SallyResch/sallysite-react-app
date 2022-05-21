dd42982ebc6cda8cf6d97d52051d14829b291d50
Sally Resch <sallyresch27@gmail.com>
2022-05-21 11:36:33 +0200

    dynamic routes

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function FirstPost() {
-  return <h1>First Post</h1>
+  return (
+    <div>
+      <h1>First Post</h1>
+      <h2>
+        <Link href='/'>
+          <a>Back to Homepage</a>
+        </Link>
+      </h2>
+    </div>
+  )
 }
