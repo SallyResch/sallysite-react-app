5ca267d49efc3a3f2a7c3318953827714bfef441
Sally Resch <sallyresch27@gmail.com>
2022-05-25 14:11:18 +0200

    Added layout styleing

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-
+import styles from './Layout.module.css';
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 }
